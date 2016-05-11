19b71999963062024d294836a5cc5e4860d94e12
tam.dang <danghatam@gmail.com>
2016-05-11 18:23:48 +0700

    second

---
+++ test/controllers/color.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+
+describe("Color API", () => {
+	describe("Get colors list", () => {
+		it("return status code 200", () => {
+
+		});
+		it("return list colors", () => {
+
+		});
+	});
+
+	describe("Get color by id", () => {
+		it("return status code 200", () => {
+
+		});
+		it("return color", () => {
+
+		});
+	});
+});
